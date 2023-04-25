4beb5737280fc3cb8109dff5aa7caf3423140684
Daniloemejias <123761185+Daniloemejias@users.noreply.github.com>
2023-04-25 03:00:37 +0000

    Algunos arreglos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,20 +3,37 @@ import React, {useState, useEffect} from "react";
 //create your first component
 const Home = () => {
 		const [todos, setTodos]=useState([])
+		const [task, setTask]=useState("")
 		const apiUrl="https://assets.breatheco.de/apis/fake/todos/user/Daniloemejias"
 
 		async function LoadList(){
-			let response=await fetch (apiUrl)
+			let response = await fetch (apiUrl)
 			if(response.ok){
-				let date=await response.json()
+				let data = await response.json()
 				setTodos(data)
 			}
 			return response.status
 		}
 
+		async function update(){
+			let response = await fetch (apiUrl,{
+				body: JSON. stringify(todos),
+				method:"PUT",
+				headers:{
+				"content-type":"application/json"
+			}
+
+			})
+			
+			if(response.ok){
+				let data = await response.json()
+				console.log(data)
+			}
+			console.log (response.status)
+		}
 
 		useEffect(()=>{
-			LoadList(async status=>{
+			LoadList().then(async status=>{
 				if(status==404){
 					let response=await fetch(apiUrl,{
 						method:"POST",
@@ -26,9 +43,9 @@ const Home = () => {
 						}
 						
 					})
-					.then((res) => {
-						console.log(res);
-						return res.json();
+					.then((response) => {
+						console.log(response);
+						return response.json();
 					})
 					.then((data) => {
 						console.log(data);
@@ -40,27 +57,29 @@ const Home = () => {
 
 		},[])
 
-		function getModal(){
-			return new bootstrap.Modal(document.getElementById('loading'))
-		}
-
 		function addTodo(e){
 			if(e.key=="Enter"){
 				//logica de agregar
-				let modal=getModal()
-				modal.show()
-				let newItem={label:e.target.value,done:false}
-				let newTodos=[...todos, newItem]
-				setTodos(newTodos)
-				e.target.value = ""
-				modal.hide()
+				// LoadList()
+				// let newItem={label:e.target.value,done:false}
+				let newTodos=[...todos, {label:task,done:false}]
+				setTodos(newTodos);
+				update(newTodos);
+				// e.target.value = ""
+				setTask("")
 			}
 		}
+
+
+
 		function deleteTodo(index){
 			//logica de borrar
+			// console.log(todos)
 			let newTodos=[...todos]
-			newTodos.splice(index,1)
+			let objIndex = newTodos.findIndex(task => task.index == index)
+			newTodos.splice(objIndex,1)
 			setTodos(newTodos)
+			update(newTodos)
 		}
 
 		function checkTodo(index){
@@ -72,9 +91,17 @@ const Home = () => {
 	return (
 		<div className="card">
 			<div className="card-header">
-				<input className="form-control border-1 mb-1" type="text" onKeyDown={addTodo}/>
+				<input 
+				className="form-control border-1 mb-1"
+				type="text"
+				onKeyDown={addTodo}
+				onChange={(e)=>setTask(e.target.value)}
+				
+				/>
+
 				<ol className="list-group list-group-flush">
-					{todos.map((todo,index)=><li key={index} className="list-group-item d-flex justify-content-between align-item-center">
+					{todos.map((todo,index)=>(
+					<li key={index} className="list-group-item d-flex justify-content-between align-item-center">
 						<div>
 							<input 
 							className="form-check-input me-3" 
@@ -84,8 +111,8 @@ const Home = () => {
 							/>
 							{todo.label}
 						</div>
-					<span onClick={()=>deleteTodo(index)} className="btn btn-outline-danger btn-sm rounded-pill">X</span>
-					</li>)}
+						<span onClick={()=>deleteTodo(index)} className="btn btn-outline-danger btn-sm rounded-pill">X</span>
+					</li>))}
 				</ol>
 			</div>
 		</div>
